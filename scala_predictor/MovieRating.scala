3c1a95afd856da00bfdd0f8f9aa2a61c34c0865f
Chris Snow <chsnow123@gmail.com>
2016-10-18 08:26:59 +0100

    Update MovieRating.scala

--- scala_predictor/MovieRating.scala
+++ scala_predictor/MovieRating.scala
@@ -9,6 +9,7 @@ import org.apache.spark.mllib.recommendation.Rating
 object MovieRating {
   def main(args: Array[String]) {
 
+    // the inputs to this spark job
     val model_path = args(0)
     val user_id = args(1).toInt
     val movie_id = args(2).toInt
@@ -17,12 +18,17 @@ object MovieRating {
     val conf = new SparkConf().setAppName("Movie Rating")
     val sc = new SparkContext(conf)
 
+    // load the saved model
     val model = MatrixFactorizationModel.load(sc, model_path)
+
+    // predict a rating for the user_id and movie_id
     val predicted_rating = model.predict(user_id, movie_id)
 
+    // package up the predicted rating so we can save it
     val rating = Rating(user_id, movie_id, predicted_rating)
-
     val response = sc.parallelize(Array(rating))
+
+    // save the predicted rating
     response.saveAsTextFile(output_path)
 
     sc.stop()
