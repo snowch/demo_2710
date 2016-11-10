08dcd272fe212b7328b07d0c97e715a9466b6cf6
christopher snow <chsnow123@gmail.com>
2016-11-10 14:30:29 +0000

    add sslify

--- web_app/app/__init__.py
+++ web_app/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask_sslify import SSLify
 
 app = Flask(__name__)
+sslify = SSLify(app)
 
 app.config.from_object('config.Config')
 
