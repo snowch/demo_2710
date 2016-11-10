175f32c92fb316320794bf18c82b6eba938bac66
christopher snow <chsnow123@gmail.com>
2016-11-10 21:42:29 +0000

    add more login boilerplate code

--- web_app/app/auth/views.py
+++ web_app/app/auth/views.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template, session, redirect, url_for
 from . import forms
 from . import auth
 from .. import app
+from .forms import LoginForm
 
 
-@auth.route('/index', methods=['GET', 'POST'])
-def index():
-
-    return render_template('/auth/index.html') 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
 
