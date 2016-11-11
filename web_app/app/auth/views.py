877242b6d2940ae03225138acd86bafa8041af69
christopher snow <chsnow123@gmail.com>
2016-11-11 08:11:57 +0000

    add registration form

--- web_app/app/auth/views.py
+++ web_app/app/auth/views.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from . import forms
 from . import auth
 from .. import app
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -15,4 +15,20 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                   'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
