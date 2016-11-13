b14247bc7981dc70d04bb31a6713c250bba505d0
christopher snow <chsnow123@gmail.com>
2016-11-13 19:45:43 +0000

    code for creating musicdb index

--- web_app/app/setup.py
+++ web_app/app/setup.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 
 from . import app
 
@@ -8,14 +8,52 @@ CL_MUSICDB  = app.config['CL_MUSICDB']
 CL_AUTHDB   = app.config['CL_AUTHDB']
 CL_RATINGDB = app.config['CL_RATINGDB']
 
-# TODO use flask logging rather than print()
-for db in [CL_MUSICDB, CL_AUTHDB, CL_RATINGDB]:
-    response = requests.get( CL_URL+'/'+db, auth=CL_AUTH )
+def create_dbs():
+    # TODO use flask logging rather than print()
+    for db in [CL_MUSICDB, CL_AUTHDB, CL_RATINGDB]:
+        response = requests.get( CL_URL+'/'+db, auth=CL_AUTH )
 
-    if not response.status_code in [200, 201, 202]:
+        if response.status_code in [200, 201, 202]:
+            print( 'Found database {0}'.format(db) )
+        else:
 
-        print( 'Creating', db )
-        response = requests.put( CL_URL+'/'+db, auth=CL_AUTH )
+            print( 'Creating', db )
+            response = requests.put( CL_URL+'/'+db, auth=CL_AUTH )
 
-        if not response.status_code in [200, 201, 202]:
-            print( 'Error creating ', db, response.text )
+            if not response.status_code in [200, 201, 202]:
+                print( 'Error creating ', db, response.text )
+
+def create_musicdb_indexes():
+    response = requests.get( CL_URL+'/'+CL_MUSICDB+'/_index', auth=CL_AUTH )
+    
+    if response.status_code in [200, 201, 202]:
+    
+        indexes = [ doc['name'] for doc in response.json()['indexes'] ]
+    
+        if 'title-artist-text' in indexes:
+            print('Found {0} title-artist-text index'.format(CL_MUSICDB))
+        else:
+            idx = {
+                "index": {
+                    "fields": [
+                        { "name": "title", "type": "string" },
+                        { "name": "artist", "type": "string" }
+                    ]
+                },
+                "name": "title-artist-text",
+                "type": "text"
+            }
+            
+            response = requests.post(
+                    CL_URL+'/'+CL_MUSICDB+'/_index', 
+                    auth=CL_AUTH, 
+                    data=json.dumps(idx), 
+                    headers={'Content-Type':'application/json'})
+    
+            if response.status_code in [200, 201, 202]:
+                print('Created {0} title-artist-text index'.format(CL_MUSICDB))
+            else:
+                print('Issue encountered creating {0} title-artist-text index: {1}'.format(CL_MUSICDB, response.text))
+    
+create_dbs()
+create_musicdb_indexes()
