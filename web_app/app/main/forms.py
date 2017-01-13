72556d9b1db06c1b19fdbb3578aa89055d795efe
christopher snow <chsnow123@gmail.com>
2017-01-13 19:49:38 +0000

    move demo to new repo

--- web_app/app/main/forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
-class SearchForm(Form):
-    search_string = StringField('Enter search string')
-    submit = SubmitField('Submit')
-
